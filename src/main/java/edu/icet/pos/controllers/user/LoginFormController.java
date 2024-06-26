51707c74e00e5f49a760049945c42014a10b1efe
Sithum <sithumsandaruwan200123@gmail.com>
2024-06-26 16:49:10 +0530

    Developing Layered

--- src/main/java/edu/icet/pos/controllers/user/LoginFormController.java
+++ src/main/java/edu/icet/pos/controllers/user/LoginFormController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -19,13 +20,27 @@ public class LoginFormController {
     private JFXTextField txtUser;
     private Stage stage;
 
+
     @FXML
     void LogInBtnOnAction(ActionEvent event) throws IOException {
 
-        Parent load = new FXMLLoader(getClass().getResource("/view/DashBoardForm.fxml")).load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(load));
-        stage.show();
+         Parent load = FXMLLoader.load(getClass().getResource("/view/DashBoardForm.fxml"));
+         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+         Scene scene = new Scene(load);
+         stage.setScene(scene);
+         stage.show();
+
+
     }
 
+    @FXML
+    public void CreateAccLinkOnAction(ActionEvent actionEvent) throws IOException {
+
+        Parent load = FXMLLoader.load(getClass().getResource("/view/SignUpForm.fxml"));
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(load);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- src/main/java/edu/icet/pos/controllers/user/SignUpFormController.java
+++ src/main/java/edu/icet/pos/controllers/user/SignUpFormController.java
@@ -1,18 +1,42 @@
 package edu.icet.pos.controllers.user;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SignUpFormController {
 
-    private Stage stage;
+    @FXML
+    private JFXTextField txtEMail;
 
-    public void RegisterBtnOnAction(ActionEvent actionEvent) throws IOException {
+    @FXML
+    private JFXTextField txtPassword;
 
+    @FXML
+    private JFXTextField txtReEnterPassword;
 
+    @FXML
+    private JFXTextField txtUser;
+
+    @FXML
+    void BackBtnOnAction(MouseEvent event) throws IOException {
+        Parent load = new FXMLLoader(getClass().getResource("/view/LogInForm.fxml")).load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(load));
+        stage.show();
     }
+
+    @FXML
+    void RegisterBtnOnAction(ActionEvent event) {
+        new Alert(Alert.AlertType.INFORMATION,"Registration Successs").show();
+    }
+
 }
